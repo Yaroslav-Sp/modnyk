a9fbdb6b3fc1ddb74e2e500e2136ba3d29f20229
Yaroslav Spisivtsev <spisivtsev19994@gmail.com>
2024-10-24 18:18:18 +0300

    Added settings

---
+++ src/config/settings/stg.py
@@ -0,0 +1,24 @@
+from config.settings.base import *  # NOQA
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-lhmdb$*b4-gvlxqaz76gc)00h*t+y6c!2a*=70ckth^awqt%*u"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []  # NOQA
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
